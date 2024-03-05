618f793b1dbafa452f4bf1181fe4ea5e2bd5883f
H82M19\Student <Student>
2024-03-05 08:21:52 +0700

    Update lan 1

--- App.js
+++ App.js
@@ -1,73 +1,191 @@
-import React, {useRef} from 'react';
-import {
-  Animated,
-  Text,
-  View,
-  StyleSheet,
-  Button,
-  SafeAreaView,
-} from 'react-native';
+// import React, { useEffect, useState } from 'react';
+// import { View, Text, Animated } from 'react-native';
+
+// const App = () => {
+//   const [opacityValue] = useState(new Animated.Value(0));
+
+//   useEffect(() => {
+//     animateText();
+//   }, []);
+
+//   const animateText = () => {
+//     Animated.sequence([
+//       Animated.timing(opacityValue, {
+//         toValue: 0,
+//         duration: 3000,
+//         useNativeDriver: false,
+//       }),
+//       Animated.timing(opacityValue, {
+//         toValue: 1,
+//         duration: 3000,
+//         useNativeDriver: false,
+//       }),
+//     ]).start();
+//   };
+
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Animated.Text
+//         style={{
+//           fontSize: 20,
+//           opacity: opacityValue,
+//         }}
+//       >
+//         Xin chào ! Tôi là Trần Đặng Hoài Nam !!!!!
+//       </Animated.Text>
+//       <View style={{ height: 20 }} />
+//       <Animated.Text
+//         style={{
+//           fontSize: 20,
+//           opacity: opacityValue,
+//         }}
+//       >
+//        <View>Tôi là sinh viên trường Đại Học Công Nghiệp thành Phố Hồ Chí Minh !!!! </View>
+//       </Animated.Text>
+//     </View>
+//   );
+// };
+
+// export default App;
+
+
+
+//cau 2
+// import React, { useRef } from 'react';
+// import { View, Text, TouchableOpacity, Animated } from 'react-native';
+
+// const App = () => {
+//   const rotation = useRef(new Animated.Value(0)).current;
+
+//   const startAnimation = () => {
+//     Animated.loop(
+//       Animated.timing(rotation, {
+//         toValue: 1,
+//         duration: 2000,
+//         useNativeDriver: true,
+//       })
+//     ).start();
+//   };
+
+//   const stopAnimation = () => {
+//     rotation.stopAnimation();
+//   };
+
+//   const restartAnimation = () => {
+//     rotation.setValue(0);
+//     startAnimation();
+//   };
+
+//   const rotateInterpolation = rotation.interpolate({
+//     inputRange: [0, 1],
+//     outputRange: ['0deg', '360deg'],
+//   });
+
+//   const animatedStyle = {
+//     width: 50,
+//     height: 50,
+//     backgroundColor: 'blue',
+//     transform: [{ rotate: rotateInterpolation }],
+//   };
+
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Animated.View style={animatedStyle} />
+//       <View style={{ marginTop: 20 }}>
+//         <TouchableOpacity onPress={startAnimation} style={styles.button}>
+//           <Text style={styles.buttonText}>Start</Text>
+//         </TouchableOpacity>
+//         <TouchableOpacity onPress={stopAnimation} style={styles.button}>
+//           <Text style={styles.buttonText}>Stop</Text>
+//         </TouchableOpacity>
+//         <TouchableOpacity onPress={restartAnimation} style={styles.button}>
+//           <Text style={styles.buttonText}>Restart</Text>
+//         </TouchableOpacity>
+//       </View>
+//     </View>
+//   );
+// };
+
+// const styles = {
+//   button: {
+//     backgroundColor: 'green',
+//     padding: 10,
+//     margin: 5,
+//     borderRadius: 5,
+//   },
+//   buttonText: {
+//     color: 'white',
+//     fontSize: 16,
+//   },
+  
+// };
+
+// export default App;
+
+
+import React, { useEffect, useRef } from 'react';
+import { View, Image, StyleSheet, Animated, Easing } from 'react-native';
 
 const App = () => {
-  // fadeAnim will be used as the value for opacity. Initial Value: 0
-  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const shakeAnimation = useRef(new Animated.Value(0)).current;
 
-  const fadeIn = () => {
-    // Will change fadeAnim value to 1 in 5 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 5000,
-      useNativeDriver: true,
-    }).start();
-  };
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(shakeAnimation, {
+          toValue: 10,
+          duration: 100,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }),
+        Animated.timing(shakeAnimation, {
+          toValue: -10,
+          duration: 100,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }),
+        Animated.timing(shakeAnimation, {
+          toValue: 0,
+          duration: 100,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }),
+      ]),
+      {
+        iterations: -1, // lặp vô hạn
+      }
+    ).start();
+  }, []);
 
-  const fadeOut = () => {
-    // Will change fadeAnim value to 0 in 3 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 0,
-      duration: 3000,
-      useNativeDriver: true,
-    }).start();
-  };
+  const interpolatedRotateAnimation = shakeAnimation.interpolate({
+    inputRange: [-10, 0, 10],
+    outputRange: ['-5deg', '0deg', '5deg'],
+  });
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Animated.View
-        style={[
-          styles.fadingContainer,
-          {
-            // Bind opacity to animated value
-            opacity: fadeAnim,
-          },
-        ]}>
-        <Text style={styles.fadingText}>Fading View!</Text>
-      </Animated.View>
-      <View style={styles.buttonRow}>
-        <Button title="Fade In View" onPress={fadeIn} />
-        <Button title="Fade Out View" onPress={fadeOut} />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Animated.Image
+        source={require('./assets/chuong.png')} 
+        style={[styles.bellImage, { transform: [{ rotate: interpolatedRotateAnimation }] }]}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    paddingRight: 20, 
+    paddingBottom: 20, 
   },
-  fadingContainer: {
-    padding: 20,
-    backgroundColor: 'powderblue',
-  },
-  fadingText: {
-    fontSize: 28,
-  },
-  buttonRow: {
-    flexBasis: 100,
-    justifyContent: 'space-evenly',
-    marginVertical: 16,
+  bellImage: {
+    width: 200, 
+    height: 200, 
   },
 });
 
 export default App;
+
+
